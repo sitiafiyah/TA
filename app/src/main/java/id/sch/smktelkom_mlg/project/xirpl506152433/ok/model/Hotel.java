fb488e11d96a1aec9f0d35c4ec728740e727a014
Siti Afiyah <sitiafiyah35@gmail.com>
2016-11-16 16:06:46 +0700

    RecyclerView List Card

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl506152433/ok/model/Hotel.java
@@ -0,0 +1,21 @@
+package id.sch.smktelkom_mlg.project.xirpl506152433.ok.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Siti Afiyah on 10/29/2016.
+ */
+
+public class Hotel {
+
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+
+}
